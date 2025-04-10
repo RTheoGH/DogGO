112062db26d11a3debb461eb29251d9b6e7c0374
tom <tomben01@laposte.net>
2025-04-11 00:50:24 +0200

    finished menu

--- src/scenes/UI/mainmenue.gd
+++ src/scenes/UI/mainmenue.gd
@@ -18,3 +18,12 @@ func _on_quit_pressed() -> void:
 func _on_options_pressed() -> void:
 	pass # Replace with function body.
 	print("Load Options Menu")
+
+func _on_start_mouse_entered() -> void:
+	$Menu.play()
+
+func _on_quit_mouse_entered() -> void:
+	$Menu.play()
+
+func _on_options_mouse_entered() -> void:
+	$Menu.play()
